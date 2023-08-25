b4ced7593dd0a15f82a07291d1274ecf00b26fc9
Oleg <86820325+AleanM@users.noreply.github.com>
2023-08-25 19:27:16 +0300

    Create ButtonComponent.vue

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="center-button-container">
+    <button class="center-button" @click="openModal">Открыть модальное окно</button>
+  </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      openModal() {
+        this.$emit('open');
+      }
+    }
+  };
+  </script>
+
+<style scoped>
+.center-button-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Высота 100% видимого окна */
+}
+
+.center-button {
+  padding: 10px 20px;
+}
+</style>

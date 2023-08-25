5bc7cb0fc3764a2403d5604b80ada8497c63b09c
Oleg <86820325+AleanM@users.noreply.github.com>
2023-08-25 19:27:36 +0300

    Add components in main app

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ButtonComponent @click="openModal" />
+    <ModalWindow  v-if="isModalOpen" @close="closeModal">
+      <p>Содержимое модального окна</p>
+    </ModalWindow >
+  </div>
+</template>
+
+<script>
+import ButtonComponent from './components/ButtonComponent.vue';
+import ModalWindow from './components/ModalWindow.vue';
+
+export default {
+  components: {
+    ModalWindow,
+    ButtonComponent
+  },
+  data() {
+    return {
+      isModalOpen: false
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalOpen = true;
+    },
+    closeModal() {
+      this.isModalOpen = false;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

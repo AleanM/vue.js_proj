405c6791326d421068e1a9aa8c1100aebb6d147c
Oleg <86820325+AleanM@users.noreply.github.com>
2023-08-26 20:06:51 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -3,16 +3,22 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
-    data: null
+    data: null,
+    totalSum: 0
   },
   mutations: {
     setData(state, payload) {
       state.data = payload;
-    }
+    },
+    setTotalSum(state, totalSum) {
+      state.totalSum = totalSum;
+    },
   },
   actions: {
     updateData({ commit }, newData) {
       commit('setData', newData);
-    }
+    },setTotalSum({ commit }, totalSum) {
+      commit('setTotalSum', totalSum);
+    },
   }
 });

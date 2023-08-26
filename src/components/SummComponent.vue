2ff94e5a46db66dceaa5eaef10cfe815aa4d2ddb
Oleg <86820325+AleanM@users.noreply.github.com>
2023-08-26 20:06:49 +0300

    Create SummComponent.vue

---
+++ src/components/SummComponent.vue
@@ -0,0 +1,14 @@
+<template>
+   <div>
+    <h1>Итого к оплате: {{ totalSum }}</h1>
+   </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+export default {
+    computed: {
+    ...mapState(['totalSum'])
+  },
+}
+</script>

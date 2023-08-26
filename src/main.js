7c2e6a8c1e7f32d88f67c41f2a11571c0f4a04c9
Oleg <86820325+AleanM@users.noreply.github.com>
2023-08-26 18:41:55 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './routes/router'; // Пример импорта маршрутизатора
+import router from './routes/router';
+import store from './store/store'; // Пример импорта маршрутизатора
+
 
 createApp(App)
   .use(router) // Убедитесь, что вы используете маршрутизатор
+  .use(store)
   .mount('#app');

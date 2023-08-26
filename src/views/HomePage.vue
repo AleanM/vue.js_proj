d6b6375ee0517693249e76f03e046b8b49eb2278
Oleg <86820325+AleanM@users.noreply.github.com>
2023-08-26 18:41:31 +0300

    init

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -14,6 +14,7 @@
   import TextTitleComponent from '../components/TextTitleComponent.vue';
   import InputComponent from '../components/InputComponent.vue';
   
+
   export default {
     components: {
       ModalWindow,
@@ -23,9 +24,10 @@
     },
     data() {
       return {
-        isModalOpen: false
+        isModalOpen: false,
       };
     },
+
     methods: {
       openModal() {
         this.isModalOpen = true;

3797aeb18e4686f86f001328498d47a9be84212c
Oleg <86820325+AleanM@users.noreply.github.com>
2023-08-26 21:44:10 +0300

    Create FinishPage.vue

---
+++ src/views/FinishPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <InfoComponent />
+</template>
+<script>
+import InfoComponent from "@/components/InfoComponents.vue"
+export default {
+ components:{
+    InfoComponent
+ }
+}
+</script>
